75bcb99210b72d8cf53396f0c25d6a83776bf1d7
Jason Sarver <96513264+Jasonsarver@users.noreply.github.com>
2022-05-08 22:13:27 -0700

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+const todos = document.getElementById("todos")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const todoText = input.value;
+    addTodo();
+
+
+    if(todoText){
+        const todoEl = document.createElement("li")
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => { todoEl.classList.toggle("completed")
+        });
+
+        todoEl.addEventListener("contextmenu", (e) =>{
+            e.preventDefault();
+            todoEl.remove();
+        })
+        todos.appendChild(todoEl);
+    }
+});
+
+function addTodo(){
+    
+}
